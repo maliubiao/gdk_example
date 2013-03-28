a973e6cef8b4c5ad04ff07f4cf253a7b16bf435c
maliubiao <maliubiao@gmail.com>
2013-03-28 16:34:22 +0800

    add event handler

--- gdk_window_new.c
+++ gdk_window_new.c
@@ -1,11 +1,56 @@
 #include <stdlib.h>
 #include <gdk/gdk.h>
 
-void delete_window_handler(GdkWindow *window, GdkEvent *event, gpointer user_data);
+void delete_window(GdkEvent *event);
+void repaint_rect(GdkEventExpose *event);
+void paint_window(GdkEventKey *event);
+void main_event_loop();
+GdkWindow* init_window();
+
 int main(int argc, char **argv)
 {
 	gdk_init(&argc,&argv);
-	//create a GdkWidnow
+	gdk_window_show(init_window());
+	main_event_loop();
+	_exit(0);
+}
+
+void main_event_loop()
+{
+	GdkEvent *event; 
+	while(1)
+	{
+		event = gdk_event_get();
+		if(event == NULL)
+			continue;
+		switch(event->type)
+		{
+			case GDK_DELETE:
+				{
+					delete_window(event);
+					break; 
+				}
+			case GDK_EXPOSE:
+				{
+					repaint_rect((GdkEventExpose *)event);
+					break;
+				}
+			case GDK_KEY_PRESS:
+				{
+					paint_window((GdkEventKey *)event);
+					break;
+				}
+			case GDK_MOTION_NOTIFY:
+				break;
+			case GDK_BUTTON_PRESS:
+				break; 
+			default:
+				break;
+		}
+	}
+}
+GdkWindow* init_window()
+{
 	GdkWindowAttr *gwa = malloc(sizeof(GdkWindowAttr));
 	gwa->title="hello world";
 	gwa->event_mask=GDK_ALL_EVENTS_MASK;
@@ -22,36 +67,47 @@ int main(int argc, char **argv)
 	gwa->override_redirect=0;
 	gwa->type_hint=GDK_WINDOW_TYPE_HINT_NORMAL;
 	GdkWindow *window;	
+	GdkRGBA color;
+	color.red=0.4;
+	color.green=0.4;
+	color.blue=0.7;
+	color.alpha=1;
 	window=gdk_window_new(NULL,gwa,GDK_WA_TITLE|GDK_WA_X|GDK_WA_Y );
-	gdk_window_show(window);
-
-	GdkEvent *event; 
-	while(1)
-	{
-		event = gdk_event_get();
-		if(event == NULL)
-			continue;
-		switch(event->type)
-		{
-			case GDK_DELETE:
-				{
-					delete_window_handler(window,event,NULL);
-					break; 
-				}
-			case GDK_MOTION_NOTIFY:
-				break;
-			case GDK_BUTTON_PRESS:
-				break;
-			case GDK_KEY_PRESS:
-				break;
-			default:
-				break;
-		}
-	}
+	gdk_window_set_background_rgba(window,&color);
+	return window;
+}
+void delete_window(GdkEvent *event)
+{
+	gdk_window_destroy(((GdkEventAny *)event)->window);
 	_exit(0);
 }
-void delete_window_handler(GdkWindow *window, GdkEvent *event, gpointer user_data)
+
+void repaint_rect(GdkEventExpose *event)
 {
-	gdk_window_destroy(window);
-	_exit(0);
+	printf("event->type: %d\n",event->type);
+	gdk_window_begin_paint_region(event->window,event->region);
+	gdk_window_end_paint(event->window);
+}
+
+void paint_window(GdkEventKey *event)
+{
+	gchar keyname[32]; 
+	cairo_t *p; 
+	g_strlcpy(keyname,gdk_keyval_name(event->keyval),sizeof(keyname));
+	printf("key pressed: %s\n",keyname);
+	if(!g_strcmp0(keyname,"p"))
+	{
+
+		p = gdk_cairo_create(event->window);
+		cairo_set_source_rgb(p,0.7,0.9,0.5);
+		cairo_set_line_width(p,32);
+		cairo_move_to(p,100,100);
+		cairo_line_to(p,400,400);
+		cairo_stroke(p);
+		cairo_destroy(p);
+	}
+
+
+		printf("don't paint\n");
+	}
 }
